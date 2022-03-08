9eb9c1565730328aa2a4b61bc200008426d0c0fe
moreza-kaze <morezakaze380@gmail.com>
2022-03-08 18:43:55 +0330

    17esfand

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div dir="rtl" class="font-main bg-gray-100">
+    <!-- start navbar & sidebar -->
+    <div id="bar">
+      <bar />
+    </div>
+    <!-- end navbar & sidebar -->
+
+    <!-------------------------->
+
+    <!-- start main component  -->
+    <div id="main">
+      <router-view />
+    </div>
+    <!-- end main component  -->
+  </div>
+</template>
+<script>
+import Bar from "./components/Bar.vue";
+export default {
+  components: {
+    Bar,
+  },
+  setup() {},
+};
+</script>
+
+<style></style>
---
+++ src/components/Bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- start sidebar w-64 -->
+  <div class="hidden sm:block w-24 h-screen bg-white">
+    <h1>fd</h1>
+  </div>
+  <!-- end sidebar -->
+  <!----------------->
+  <!-- start sidebar -->
+  <div></div>
+  <!-- end sidebar -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
