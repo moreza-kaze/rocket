f2b5a267c38f6433c745b10cf954daec6ab7a869
moreza-kaze <morezakaze380@gmail.com>
2022-03-27 16:14:57 +0430

    search user form on 7farvardin1401

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,5 +1,73 @@
 <template>
-  <h1 class="mt-24">usersShow.vue</h1>
+  <div class="m-5 h-20 sm:h-14">
+    <div class="w-full flex items-center justify-between">
+      <div class="flex items-center justify-center text-xl">
+        <span class="mt-1"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-7 w-7"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+            /></svg
+        ></span>
+        <span class="mr-3 font-medium text-2xl">مدیریت کاربران</span>
+      </div>
+      <div class="flex flew-row justify-center items-center">
+        <span
+          ><form
+            @submit.prevent=""
+            class="relative ml-1 md:ml-2 flex items-center"
+          >
+            <input type="text" class="h-9 w-56 rounded-2xl" /><button
+              class="absolute left-0 top-0 mt-1 ml-1"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-7 w-7"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1.5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button></form
+        ></span>
+        <span class="hidden md:inline"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1.5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            /></svg
+        ></span>
+      </div>
+    </div>
+    <div>
+      <table>
+        <thead></thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
