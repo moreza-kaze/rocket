2b45748b0a06d9e8a47923d4e8ae12ae31d5a635
moreza-kaze <morezakaze380@gmail.com>
2022-04-07 12:33:32 +0430

    show test user 18farvardin1401

--- src/views/users/UsersShow.vue
+++ src/views/users/UsersShow.vue
@@ -135,8 +135,13 @@
           </div>
           <!-- table head on sm(640PX) ens  -->
         </div>
-        <!-- orginal table -->
-        <div name="tmain2" class="flex flex-row sm:flex-col justify-around">
+        <!-- orginal table  -->
+        <div
+          name="tmain2"
+          v-for="(user, index) in users"
+          :key="index"
+          class="flex flex-row sm:flex-col justify-around"
+        >
           <!-- table head -->
           <div
             name="thead"
@@ -217,43 +222,43 @@
                 name="th"
                 class="py-1 mx-1 mb-2 mr-2 sm:mb-0 rounded-md w-11/12 sm:w-1/12 text-center overflow-x-hidden bg-maincolor-900 text-white"
               >
-                1
+                {{ user.id }}
               </div>
               <div
                 name="th"
                 class="py-1 mx-1 mb-2 sm:mb-0 rounded-md w-11/12 sm:w-2/12 text-center overflow-x-hidden bg-maincolor-900 text-white"
               >
-                1
+                {{ user.fullName }}
               </div>
               <div
                 name="th"
                 class="py-1 mx-1 mb-2 sm:mb-0 rounded-md w-11/12 sm:w-2/12 text-center overflow-x-hidden bg-maincolor-900 text-white"
               >
-                1
+                {{ user.mobile_number }}
               </div>
               <div
                 name="th"
                 class="py-1 mx-1 mb-2 sm:mb-0 rounded-md w-11/12 sm:w-2/12 text-center overflow-x-hidden bg-maincolor-900 text-white"
               >
-                1
+                {{ user.username }}
               </div>
               <div
                 name="th"
                 class="py-1 mx-1 mb-2 sm:mb-0 rounded-md w-11/12 sm:w-2/12 text-center overflow-x-hidden bg-maincolor-900 text-white"
               >
-                1
+                {{ user.password }}
               </div>
               <div
                 name="th"
                 class="py-1 mx-1 mb-2 sm:mb-0 rounded-md w-11/12 sm:w-2/12 text-center overflow-x-hidden bg-maincolor-900 text-white"
               >
-                1
+                {{ user.invoice_id }}
               </div>
               <div
                 name="th"
                 class="py-1 mx-1 mb-2 sm:mb-0 rounded-md w-11/12 sm:w-2/12 text-center overflow-x-hidden bg-maincolor-900 text-white"
               >
-                1
+                {{ user.payment_result }}
               </div>
               <div
                 name="th"
@@ -274,7 +279,82 @@
 </template>
 
 <script>
-export default {};
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const users = ref();
+    users.value = [
+      {
+        id: 16,
+        fullName: "Prof. Lucious",
+        mobile_number: "+1-323-529-1054",
+        username: "sidney61",
+        password: '"qCf)&b',
+        invoice_id: 16,
+        payment_result: "Nemo.",
+        coupon_id: 23,
+        status: "Natus qui.",
+      },
+      {
+        id: 12,
+        fullName: "Dr. Ivory II",
+        mobile_number: "1-562-647-3688",
+        username: "wehner.jocelyn",
+        password: "nqn`MG'')",
+        invoice_id: 6,
+        payment_result: "Veniam.",
+        coupon_id: 21,
+        status: "Dolorum.",
+      },
+      {
+        id: 5,
+        fullName: "Vincenzo Johns",
+        mobile_number: "+1.463.760.9935",
+        username: "kenya.buckridge",
+        password: "sk?YO/",
+        invoice_id: 11,
+        payment_result: "Labore.",
+        coupon_id: 35,
+        status: "Dicta.",
+      },
+      {
+        id: 56,
+        fullName: "Idella Fritsch",
+        mobile_number: "1-657-387-3204",
+        username: "chyna36",
+        password: "_,zsyRh'+x",
+        invoice_id: 27,
+        payment_result: "Ipsam.",
+        coupon_id: 16,
+        status: "Vel et.",
+      },
+      {
+        id: 34,
+        fullName: "Fredrick Schumm",
+        mobile_number: "+1 (484) 258-9273",
+        username: "parisian.marisa",
+        password: "C%<x'39,`,W*R",
+        invoice_id: 43,
+        payment_result: "Tenetur.",
+        coupon_id: 2,
+        status: "Ducimus.",
+      },
+      {
+        id: 29,
+        fullName: "Caden Schowalter",
+        mobile_number: "+1.385.601.6459",
+        username: "salvatore72",
+        password: "M9WfxrJ8'N7WzetIN=*",
+        invoice_id: 50,
+        payment_result: "Quaerat.",
+        coupon_id: 13,
+        status: "Nostrum.",
+      },
+    ];
+    console.log(users);
+    return { users };
+  },
+};
 </script>
 
 <style scoped></style>
