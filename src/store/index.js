e69af4e7a81c086d4349f79f80c8a3621fe51bb1
moreza-kaze <morezakaze380@gmail.com>
2022-04-26 17:33:02 +0430

    connect to backend  6ordibehesht1401

--- src/store/index.js
+++ src/store/index.js
@@ -6,14 +6,16 @@ export default createStore({
     menu: false,
     dashboard: {},
     usersLastPage: 1,
+    couponsLastPage: 1,
     users: [],
     smsGateway: [],
+    coupons: [],
   },
   getters: {
     getMenu(state) {
       return state.menu;
     },
-    getUser(state) {
+    getDataUser(state) {
       return state.users;
     },
     getDashboard(state) {
@@ -21,6 +23,9 @@ export default createStore({
     },
     getsmsGatewaySetting(state) {
       return state.smsGateway;
+    },
+    getDataCoupon(state) {
+      return state.coupons;
     },
   },
   mutations: {
@@ -32,7 +37,7 @@ export default createStore({
     getUserLastPage(state, val) {
       state.usersLastPage = val;
     },
-    getUser(state, val) {
+    getDataUser(state, val) {
       state.users = [];
       state.users = val;
     },
@@ -44,24 +49,55 @@ export default createStore({
     getsmsGatewaySetting(state, val) {
       state.smsGateway = val;
     },
+    getDataCoupon(state, val) {
+      state.coupons = [];
+      state.coupons = val.data;
+      state.couponsLastPage = val.last_page;
+    },
   },
   actions: {
     actionMenu({ commit }, val) {
       commit("mutationMenu", val);
     },
+
     // get users //
-    async getUser({ commit }, val) {
+    async getDataUser({ commit }, val) {
       const response = await axios.get(
         `${apiurl}/api/admin/customers?page=${val}`
       );
       // console.log(response.data.data.output.data);
-      commit("getUser", response.data.data.output.data);
+      commit("getDataUser", response.data.data.output.data);
       commit("getUserLastPage", response.data.data.output.last_page);
     },
+
     // get dashboard data //
     async getDataDashboard({ commit }) {
       const response = await axios.get(`${apiurl}/api/admin/home`);
       commit("getDashboard", response.data);
+    },
+
+    // get coupon data
+    async getDataCoupon({ commit }, val) {
+      const response = await axios.get(
+        `${apiurl}/api/admin/coupons?page=${val}`
+      );
+      commit("getDataCoupon", response.data.data.output);
+    },
+    async createcoupon({ commit }, val) {
+      console.log(val);
+      const response = await axios.post(`${apiurl}/api/admin/coupons`, {
+        coupon_name: val.coupon_name,
+        active: Number(val.active),
+        total_amount: Number(val.total_amount),
+        discount_type: val.discount_type,
+        discount_value: Number(val.discount_value),
+      });
+      if (response.data.status != 4) {
+        console.log(response.data.data.message);
+      } else {
+        console.log(response.data.data.message);
+      }
+      return { commit };
     },
     // get sms gateway setting
     async getsmsGatewaySetting({ commit }) {
