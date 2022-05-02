669daf7ed3897d39f77a49e520615b6473f6f544
moreza-kaze <morezakaze380@gmail.com>
2022-05-02 15:16:35 +0430

    finish product 11ordibehesht

---
+++ src/views/product/ProductEdit.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="h-full w-full flex flex-col">
+    <div class="w-full flex items-center justify-between h-20 sm:h-14 m-4">
+      <div class="flex items-center justify-center text-xl">
+        <span class="mt-1"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-7 w-7"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1.5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            /></svg
+        ></span>
+        <span class="mr-3 font-medium text-2xl">اصلاح محصول</span>
+      </div>
+    </div>
+    <div
+      class="flex w-full justify-center items-center px-3 md:px-16 h-full mb-10"
+    >
+      <div class="w-full h-full bg-whitetransparent-500 rounded-2xl">
+        <form
+          @submit.prevent="updateProduct()"
+          class="w-full h-full flex flex-col md:flex-row justify-around items-center mt-0 p-10"
+        >
+          <div
+            class="h-full w-full md:w-5/6 flex flex-col justify-between items-center md:items-start"
+          >
+            <input
+              v-model="products.product_name"
+              class="bg-product w-11/12 h-10 md:h-11 2xl:3/12 pr-3 md:pr-5 placeholder:text-gray-400 border border-white rounded-xl mt-3 md:mt-0 p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="نام‌ محصول"
+              type="text"
+            />
+            <input
+              disabled
+              v-model="products.product_id"
+              class="bg-product w-11/12 h-10 md:h-11 2xl:3/12 pr-3 md:pr-5 placeholder:text-gray-400 border border-white rounded-xl mt-3 md:mt-0 p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="کد محصول"
+              type="text"
+            />
+            <input
+              v-model="products.amount"
+              class="bg-product w-11/12 h-10 md:h-11 2xl:h-3/12 pr-3 md:pr-5 placeholder:text-gray-400 border border-white rounded-xl mt-3 md:mt-0 p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="هزینه اصلی"
+              type="text"
+            />
+            <input
+              v-model="products.amount_final"
+              class="bg-product w-11/12 h-10 md:h-11 2xl:h-3/12 pr-3 md:pr-5 placeholder:text-gray-400 border border-white rounded-xl p-2 mt-3 md:mt-0 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="هزینه با تخفیف"
+              type="text"
+            />
+            <input
+              v-model="products.product_sms"
+              class="appearance-none bg-product h-10 md:h-11 2xl:h-3/12 w-11/12 pr-3 md:pr-5 placeholder:text-gray-400 border border-white rounded-xl mt-3 md:mt-0 p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="تعداد پیامک"
+              type="text"
+            />
+            <input
+              v-model="products.product_gift"
+              class="appearance-none bg-product h-10 md:h-11 2xl:h-3/12 w-11/12 pr-3 md:pr-5 placeholder:text-gray-400 border border-white rounded-xl mt-3 md:mt-0 p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="تعداد پیامک هدیه"
+              type="text"
+            />
+            <input
+              v-model="products.sms_price"
+              class="appearance-none bg-product h-10 md:h-11 2xl:h-3/12 w-11/12 pr-3 md:pr-5 placeholder:text-gray-400 border border-white rounded-xl mt-3 md:mt-0 p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="تعرفه"
+              type="text"
+            />
+          </div>
+          <div
+            class="h-96 md:h-full my-5 w-full md:w-9/12 bg-white rounded-lg flex flex-col justify-around items-center p-4"
+          >
+            <span class="text-gray-500"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-20 w-20"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+                /></svg
+            ></span>
+            <span class="text-maincolor-999 text-xl md:text-4xl">{{
+              products.product_name
+            }}</span>
+            <span class="text-maincolor-999 text-lg md:text-2xl">{{
+              products.amount_final
+            }}</span>
+            <span class="text-maincolor-999 text-base md:text-xl">{{
+              products.product_gift
+            }}</span>
+            <span class="text-maincolor-999 text-sm md:text-lg">{{
+              products.sms_price
+            }}</span>
+            <button
+              class="bg-gradient-to-r from-green-500 to-emerald-400 hover:from-emerald-600 hover:to-green-600 h-10 text-white rounded-xl w-9/12"
+            >
+              ثبت محصول
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from "@vue/reactivity";
+import { useStore } from "vuex";
+import Swal from "sweetalert2";
+import { useRoute } from "vue-router";
+import { computed, watchEffect } from "@vue/runtime-core";
+export default {
+  setup() {
+    const route = useRoute();
+    const store = useStore();
+    const products = reactive({
+      product_id: "",
+      product_name: "",
+      amount: "",
+      amount_final: "",
+      product_sms: "",
+      product_gift: "",
+      sms_price: "",
+    });
+
+    store.dispatch("getOneProduct", route.params.id);
+
+    const oneProduct = computed(() => store.state.OneProduct);
+
+    watchEffect(() => {
+      if (oneProduct.value == undefined) {
+        store.dispatch("getOneProduct", route.params.id);
+        console.log("again");
+      } else {
+        products.product_id = route.params.id;
+        products.product_name = oneProduct.value.product_name;
+        products.amount = oneProduct.value.amount;
+        products.amount_final = oneProduct.value.amount_final;
+        products.product_sms = oneProduct.value.product_sms;
+        products.product_gift = oneProduct.value.product_gift;
+        products.sms_price = oneProduct.value.sms_price;
+      }
+    });
+    const updateProduct = () => {
+      console.log(products);
+      if (
+        products.product_name.length > 0 &&
+        products.amount > 0 &&
+        products.amount_final > 0 &&
+        products.product_id > 0 &&
+        products.sms_price.length > 0
+      ) {
+        store.dispatch("updateProduct", [route.params.id, products]);
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "...خطا",
+          text: "مقادیر را به صورت کامل وارد نمایید",
+        });
+      }
+    };
+
+    return {
+      products,
+      updateProduct,
+    };
+  },
+};
+</script>
---
+++ src/views/product/ProductShow.vue
@@ -0,0 +1,310 @@
+<template>
+  <div class="m-4 sm:m-0 sm:px-6 sm:mt-4 min-h-full flex flex-col">
+    <div class="w-full flex items-center justify-between h-20 sm:h-14">
+      <div class="flex items-center justify-center text-xl">
+        <span class=""
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-7 w-7"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1.5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+            /></svg
+        ></span>
+        <span class="mr-3 font-medium text-lg md:text-2xl">محصولات</span>
+      </div>
+      <div class="flex flew-row justify-center items-center">
+        <span></span>
+        <span class="hidden md:inline"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1.5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            /></svg
+        ></span>
+      </div>
+    </div>
+    <!-- start user table  -->
+    <div class="flex justify-center items-center mt-1 p-1">
+      <!-- make table with div -->
+      <div name="table" class="flex flex-col w-full h-full">
+        <!-- main of table -->
+        <div name="tmain1" class="flex flex-row sm:flex-col shadow-xl">
+          <!-- table head on sm(640PX)  -->
+          <div
+            name="thead"
+            class="sm:flex flex-col w-1/2 hidden sm:w-full rounded-lg font-semibold sm:rounded-none sm:rounded-t-lg bg-product py-2 border-solid border-gray-300"
+          >
+            <!-- table row -->
+            <div
+              name="tr"
+              class="w-full flex flex-col sm:flex-row items-center justify-around"
+            >
+              <!-- table col  -->
+              <div
+                name="th"
+                class="sm:bg-transparent py-1 mx-1 mr-2 rounded-md w-1/12 text-center overflow-x-hidden"
+              >
+                کد محصول
+              </div>
+              <div
+                name="th"
+                class="sm:bg-transparent py-1 mx-1 rounded-md w-2/12 text-center overflow-x-hidden"
+              >
+                نام محصول
+              </div>
+              <div
+                name="th"
+                class="sm:bg-transparent py-1 mx-1 rounded-md w-2/12 text-center overflow-x-hidden"
+              >
+                مبلغ
+              </div>
+              <div
+                name="th"
+                class="sm:bg-transparent py-1 mx-1 rounded-md w-2/12 text-center overflow-x-hidden"
+              >
+                پیامک
+              </div>
+              <div
+                name="th"
+                class="sm:bg-transparent py-1 mx-1 rounded-md w-2/12 text-center overflow-x-hidden"
+              >
+                پیامک هدیه
+              </div>
+              <div
+                name="th"
+                class="sm:bg-transparent py-1 mx-1 rounded-md w-2/12 text-center overflow-x-hidden"
+              >
+                تعرفه
+              </div>
+              <div
+                name="th"
+                class="sm:bg-transparent py-1 mx-1 ml-2 rounded-md w-3/12 text-center overflow-x-hidden"
+                text-center
+              >
+                عملیات
+              </div>
+              <!-- table col end  -->
+            </div>
+            <!-- table row end -->
+          </div>
+          <!-- table head on sm(640PX) ens   -->
+        </div>
+        <!-- orginal table  -->
+        <div
+          v-for="(product, index) in products"
+          :key="index"
+          name="tmain2"
+          class="flex flex-row sm:flex-col justify-around my-2 sm:my-0"
+        >
+          <!-- table head -->
+          <div
+            name="thead"
+            class="sm:hidden flex flex-col w-5/12 sm:w-full sm:mb-2.5 bg-product py-2 rounded-xl"
+          >
+            <!-- table row -->
+            <div
+              name="tr"
+              class="w-full flex flex-col sm:flex-row items-center justify-around"
+            >
+              <!-- table col  -->
+              <div
+                name="th"
+                class="py-1 px-4 rounded-lg w-11/12 mb-2 overflow-x-hidden"
+              >
+                کد محصول
+              </div>
+              <div
+                name="th"
+                class="py-1 px-4 rounded-lg w-11/12 mb-2 overflow-x-hidden"
+              >
+                نام محصول
+              </div>
+              <div
+                name="th"
+                class="py-1 px-4 rounded-lg w-11/12 mb-2 overflow-x-hidden"
+              >
+                مبلغ
+              </div>
+              <div
+                name="th"
+                class="py-1 px-4 rounded-lg w-11/12 mb-2 overflow-x-hidden"
+              >
+                پیامک
+              </div>
+              <div
+                name="th"
+                class="py-1 px-4 rounded-lg w-11/12 mb-2 overflow-x-hidden"
+              >
+                پیامک هدیه
+              </div>
+              <div
+                name="th"
+                class="py-1 px-4 rounded-lg w-11/12 mb-2 overflow-x-hidden"
+              >
+                تعرفه
+              </div>
+              <div
+                name="th"
+                class="py-1 px-4 rounded-lg w-11/12 overflow-x-hidden"
+              >
+                عملیات
+              </div>
+              <!-- table col  -->
+            </div>
+            <!-- table row  -->
+          </div>
+          <!-- table head end -->
+          <!--------------------->
+          <!-- table body -->
+          <div
+            name="tbody"
+            class="flex flex-col w-6/12 sm:w-full bg-white py-1.5 rounded-xl sm:rounded-none border-0 sm:border-t-2 border-solid border-gray-300"
+          >
+            <!-- table row -->
+            <div
+              name="tr"
+              class="w-full flex flex-col sm:flex-row items-center justify-around"
+            >
+              <!-- table col -->
+              <div
+                name="th"
+                class="py-1 mx-1 mb-2 mr-1 sm:mb-0 w-11/12 sm:w-1/12 text-center overflow-x-hidden sm:bg-transparent text-gray-600"
+              >
+                {{ product.product_id }}
+              </div>
+              <div
+                name="th"
+                class="py-1 mx-1 mb-2 sm:mb-0 border-t-2 sm:border-0 border-solid border-gray-300 w-11/12 sm:w-2/12 text-center overflow-x-hidden sm:bg-transparent text-gray-600"
+              >
+                {{ product.product_name }}
+              </div>
+              <div
+                name="th"
+                class="py-1 mx-1 mb-2 sm:mb-0 border-t-2 sm:border-0 border-solid border-gray-300 w-11/12 sm:w-2/12 text-center overflow-x-hidden sm:bg-transparent text-gray-600"
+              >
+                <del class="text-red-300 text-xs">{{ product.amount }}</del>
+                {{ product.amount_final }}تومان
+              </div>
+              <div
+                name="th"
+                class="py-1 mx-1 mb-2 sm:mb-0 border-t-2 sm:border-0 border-solid border-gray-300 w-11/12 sm:w-2/12 text-center overflow-x-hidden sm:bg-transparent text-gray-600"
+              >
+                {{ product.product_sms }}
+              </div>
+              <div
+                name="th"
+                class="py-1 mx-1 mb-2 sm:mb-0 border-t-2 sm:border-0 border-solid border-gray-300 w-11/12 sm:w-2/12 text-center overflow-x-hidden sm:bg-transparent text-gray-600"
+              >
+                {{ product.product_gift }}
+              </div>
+              <div
+                name="th"
+                class="py-1 mx-1 mb-2 sm:mb-0 border-t-2 sm:border-0 border-solid border-gray-300 w-11/12 sm:w-2/12 text-center overflow-x-hidden sm:bg-transparent text-gray-600"
+              >
+                {{ product.sms_price }}
+              </div>
+              <div
+                name="th"
+                class="py-1 mx-1 mb-0 sm:ml-2 sm:mb-0 border-t-2 sm:border-0 border-solid border-gray-300 w-11/12 sm:w-3/12 text-center overflow-x-hidden sm:bg-transparent text-gray-600 flex flex-row justify-evenly"
+              >
+                <router-link
+                  :to="{
+                    name: 'productedit',
+                    params: { id: product.product_id },
+                    props: { product: product },
+                  }"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </router-link>
+                <button @click="deleteProduct(product.product_id)">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <!-- table col end -->
+            </div>
+            <!-- table row end -->
+          </div>
+          <!-- table body end-->
+        </div>
+        <!-- orginal table end -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from "@vue/runtime-core";
+import { useStore } from "vuex";
+import Swal from "sweetalert2";
+export default {
+  setup() {
+    const store = useStore();
+    store.dispatch("getproduct");
+
+    const products = computed(() => store.state.products);
+    const deleteProduct = (id) => {
+      Swal.fire({
+        title: "از حذف محصول اطمینان دارید؟",
+        icon: "question",
+        iconHtml: "!",
+        confirmButtonText: "بله",
+        cancelButtonText: "خیر",
+        showCancelButton: true,
+        showCloseButton: true,
+        confirmButtonColor: "#dc3741",
+        cancelButtonColor: "rgb(83 177 30)",
+        preConfirm: () => {
+          return store.dispatch("deleteProduct", Number(id));
+        },
+      });
+    };
+    return {
+      products,
+      deleteProduct,
+    };
+  },
+};
+</script>
+
+<style></style>
